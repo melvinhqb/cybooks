f9334a537cc8719d9b54fb59a19cf0779c9abd74
Dawid_Malicki <dawird.m@gmail.com>
2024-05-15 19:36:58 +0200

    updatedBook

--- src/main/java/fr/cyu/cybooks/dao/api/BookParser.java
+++ src/main/java/fr/cyu/cybooks/dao/api/BookParser.java
@@ -13,9 +13,10 @@ import java.util.List;
 public class BookParser {
 
     public static List<Book> parseBooksFromResponse(String xmlResponse) throws Exception {
+        System.out.println(xmlResponse);
         Document doc = parseXmlResponse(xmlResponse);
-        NodeList recordList = doc.getElementsByTagNameNS("http://www.loc.gov/zing/srw/", "record");
 
+        NodeList recordList = doc.getElementsByTagNameNS("http://www.loc.gov/zing/srw/", "record");
         List<Book> books = new ArrayList<>();
 
         for (int i=0; i<recordList.getLength(); i++) {
@@ -33,13 +34,40 @@ public class BookParser {
         String recordIdentifier = parseRecordIdentifier(recordElement);
         book.setId(recordIdentifier);
 
-        NodeList dataFieldList = recordElement.getElementsByTagNameNS("info:lc/xmlns/marcxchange-v2", "datafield");
+        NodeList dataFieldList = recordElement.getElementsByTagName("oai_dc:dc");
         for (int j=0; j < dataFieldList.getLength(); j++) {
-            Element datafieldElement = (Element) dataFieldList.item(j);
-            updateBookDetailsFromDataField(book, datafieldElement);
+            Element dataField = (Element) dataFieldList.item(j);
+            // Extract and update book details here
+            String bookinfo = extractTextContent(dataField, "dc:title");
+            assert bookinfo != null;
+            String title=extractTitle(bookinfo);
+            String author=extractAuthor(bookinfo);
+            System.out.println("title: " + title);
+            System.out.println("author: " + author);
+            // Set book details
+            book.setTitle(title);
+            book.setAuthor(author);
         }
 
         return book;
+    }
+
+    private static String extractTitle(String bookInfo) {
+        int slashIndex = bookInfo.indexOf('/');
+        int parenthesisIndex = bookInfo.indexOf('(');
+        int endIndex = Math.min(slashIndex != -1 ? slashIndex : Integer.MAX_VALUE,
+                parenthesisIndex != -1 ? parenthesisIndex : Integer.MAX_VALUE);
+        return bookInfo.substring(0, endIndex).trim();
+    }
+
+    private static String extractAuthor(String bookInfo) {
+        int slashIndex = bookInfo.indexOf('/');
+        int parenthesisIndex = bookInfo.indexOf(')');
+        int semicolonIndex = bookInfo.indexOf(';');
+        int startIndex = Math.max(slashIndex != -1 ? slashIndex + 1 : 0,
+                parenthesisIndex != -1 ? parenthesisIndex + 1 : 0);
+        int endIndex = semicolonIndex != -1 ? semicolonIndex : bookInfo.length();
+        return bookInfo.substring(startIndex, endIndex).trim();
     }
 
     public static String parseRecordIdentifier(Element recordElement) {
@@ -53,17 +81,12 @@ public class BookParser {
         return recordIdentifier;
     }
 
-    private static void updateBookDetailsFromDataField(Book bookInfo, Element dataField) {
-        String tag = dataField.getAttribute("tag");
-        NodeList subfields = dataField.getElementsByTagNameNS("info:lc/xmlns/marcxchange-v2", "subfield");
-        for (int i = 0; i < subfields.getLength(); i++) {
-            Element subfield = (Element) subfields.item(i);
-            String code = subfield.getAttribute("code");
-            String value = subfield.getTextContent();
-            if (tag.equals("200")) {
-                if (code.equals("a")) bookInfo.setTitle(value);
-            }
+    private static String extractTextContent(Element parentElement, String tagName) {
+        NodeList nodeList = parentElement.getElementsByTagName(tagName);
+        if (nodeList.getLength() > 0) {
+            return nodeList.item(0).getTextContent();
         }
+        return null;
     }
 
     private static Document parseXmlResponse(String xml) throws Exception {
