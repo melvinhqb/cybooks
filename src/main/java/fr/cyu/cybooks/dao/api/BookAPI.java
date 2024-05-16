b48c5f5fe1af2f86a8986d2a6c7a3ed67109490a
Dawid_Malicki <dawird.m@gmail.com>
2024-05-16 02:34:35 +0200

    update

--- src/main/java/fr/cyu/cybooks/dao/api/BookAPI.java
+++ src/main/java/fr/cyu/cybooks/dao/api/BookAPI.java
@@ -14,7 +14,9 @@ import fr.cyu.cybooks.models.Book;
 public class BookAPI {
     private final HttpClient client;
     Map<String, String> map = new HashMap<>();
-
+    Integer index = 1;
+    Integer max = null;
+    Integer jump = 10;
 
     public BookAPI() {
         this.client = HttpClient.newBuilder()
@@ -24,11 +26,22 @@ public class BookAPI {
 
     public List<Book> searchBooksByMap() {
         try {
-            URI uri = buildSearchUri(this.mapToString());
-            HttpRequest request = createHttpRequest(uri);
-            HttpResponse<String> response = sendHttpRequest(request);
-            if (response.statusCode() == 200) {
-                return BookParser.parseBooksFromResponse(response.body());
+            if (jump > 0 && index > 0) {
+                URI uri = buildSearchUri(this.mapToString(), index.toString(), jump.toString());
+                HttpRequest request = createHttpRequest(uri);
+                HttpResponse<String> response = sendHttpRequest(request);
+                if (max == null){
+                    max = BookParser.getNumberResults(response.body());
+                    System.out.println("YES HAHAHAHHAHAHAHAH" +max);
+                }else if (max < index){
+                    return null;
+                }
+                if (response.statusCode() == 200) {
+                    index+=jump;
+                    return BookParser.parseBooksFromResponse(response.body());
+                }
+            } else {
+                return null;
             }
         } catch (Exception e) {
             System.err.println("Error during HTTP request: " + e.getMessage());
@@ -61,24 +74,24 @@ public class BookAPI {
         }
     }
 
-    private URI buildSearchUri(String keyword) throws Exception {
+    private URI buildSearchUri(String keyword, String index, String jump) throws Exception {
         String base = "https://catalogue.bnf.fr/api/SRU";
         String query = String.format("bib.frenchNationalBibliography all \"Books\" and bib.language any \"fre\" and %s", keyword);
-        return buildUri(base, query);
+        return buildUri(base, query, index, jump);
     }
 
 
     private URI buildFetchUri(String bookId) throws Exception {
         String base = "https://catalogue.bnf.fr/api/SRU";
         String query = String.format("bib.persistentid=\"%s\"", bookId);
-        return buildUri(base, query);
+        return buildUri(base, query, "1","1");
     }
 
-    private URI buildUri(String base, String query) throws Exception {
+    private URI buildUri(String base, String query, String index, String jump) throws Exception {
         String version = "version=1.2";
         String operation = "operation=searchRetrieve";
         String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
-        String fullUrl = String.format("%s?%s&%s&query=%s&recordSchema=dublincore", base, version, operation, encodedQuery);
+        String fullUrl = String.format("%s?%s&%s&query=%s&recordSchema=dublincore&maximumRecords=%s&startRecord=%s", base, version, operation, encodedQuery, jump, index);
         return URI.create(fullUrl);
     }
 
@@ -92,10 +105,18 @@ public class BookAPI {
         } else if (Objects.equals(condition, "genre")) {
             this.map.put("bib.otherid all ", "\""+message+"\"");
         }
+        index = 1;
+        max=null;
     }
 
-    private void clearFilter(){
+    public void clearFilter(){
         this.map.clear();
+        index = 1;
+        max=null;
+    }
+
+    public boolean isFilterEmpty(){
+        return this.map.isEmpty();
     }
 
     private String mapToString(){
@@ -118,5 +139,25 @@ public class BookAPI {
 
     private HttpResponse<String> sendHttpRequest(HttpRequest request) throws Exception {
         return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public Integer index() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    public void setJump(Integer jump) {
+        this.jump = jump;
+    }
+
+    public Integer getJump() {
+        return jump;
     }
 }
