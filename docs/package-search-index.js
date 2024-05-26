0e52cf810c61b61b39ec78b04b533b316f9850b5
Melvin <melvinhqb@cy-tech.fr>
2024-05-26 12:54:24 +0200

    add javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.cyu.cybooks"},{"l":"fr.cyu.cybooks.connection"},{"l":"fr.cyu.cybooks.dao"},{"l":"fr.cyu.cybooks.dao.api"},{"l":"fr.cyu.cybooks.dao.implement"},{"l":"fr.cyu.cybooks.models"},{"l":"fr.cyu.cybooks.view"}];updateSearchResults();
